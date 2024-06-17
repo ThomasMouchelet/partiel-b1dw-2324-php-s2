bedb19751866be3e4ef128a48d5ea16097214714
Pitra <justin.pitra@gmail.com>
2024-06-17 14:21:15 +0200

    ✨Show posts

--- src/index.php
+++ src/index.php
@@ -1,11 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Partiel B1 DW 2324 PHP S2</h1>
-</body>
-</html>
+<?php
+require_once __DIR__ . '/parts/header.php';
+
+try {
+    $db_connect = new PDO("mysql:host=db;dbname=wordpress", "root", "admin");
+    $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $request = $db_connect->query("SELECT * FROM post");
+    $posts = $request->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo 'Erreur de connexion : ' . $e->getMessage();
+    exit;
+}
+?>
+<h1>Liste des Posts</h1>
+<?php if (!empty($posts)): ?>
+    <?php foreach ($posts as $post): ?>
+        <div class="billet">
+            <div class="main-content">
+                <div class="title">
+                    <?php echo($post['categorie']); ?>
+                </div>
+                <div class="infos">
+                    <?php echo($post['groupe']); ?> - <?php echo($post['equipe1']); ?> vs <?php echo($post['equipe2']); ?> : <?php echo($post['description']); ?>
+                </div>
+                <div class="date_heure">
+                    <?php echo($post['date_heure']); ?> | <?php echo($post['lieu']); ?>
+                </div>
+            </div>
+            <div class="price">
+                <?php echo($post['prix']); ?> €
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php else: ?>
+    <p>Aucun post trouvé</p>
+<?php endif; ?>
+
+<?php
+require_once __DIR__ . '/parts/footer.php';
+?>
